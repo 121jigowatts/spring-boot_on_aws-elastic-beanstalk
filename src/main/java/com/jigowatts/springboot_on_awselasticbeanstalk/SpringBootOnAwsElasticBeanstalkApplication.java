ae8cbec8711711acf7a84bb3c71ccdd38a498f23
soil <sh.yoshida@live.jp>
2021-02-07 23:26:38 +0900

    init

---
+++ src/main/java/com/jigowatts/springboot_on_awselasticbeanstalk/SpringBootOnAwsElasticBeanstalkApplication.java
@@ -0,0 +1,13 @@
+package com.jigowatts.springboot_on_awselasticbeanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnAwsElasticBeanstalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnAwsElasticBeanstalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jigowatts/springboot_on_awselasticbeanstalk/SpringBootOnAwsElasticBeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jigowatts.springboot_on_awselasticbeanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOnAwsElasticBeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
